609bded170e2ee6606cd02efbcaa1f6469881710
Brian Wuest <brian.wuest@outlook.com>
2022-08-04 20:05:03 -0500

    Adding comments to build.rs

--- server/build.rs
+++ server/build.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::path::Path;
 
 fn main() {
+	// Copy the default json from the root. This file contains database and web configuration information.
 	match env::var("CARGO_MANIFEST_DIR") {
 		Ok(path_string) => {
 			let output_dir = Path::new(&path_string).join("target").join(env::var("PROFILE").unwrap());
@@ -9,6 +10,7 @@ fn main() {
 
 			let output_path = Path::new(&output_dir).join("default.json");
 
+			// Always overwrite the file in the build directory.
 			let res = std::fs::copy(input_path, output_path);
 
 			match res {

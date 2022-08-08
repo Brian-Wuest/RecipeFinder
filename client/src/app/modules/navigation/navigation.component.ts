f0685cf203124474dc6bbe611b6c11d72c3dfc31
Brian Wuest <brian.wuest@outlook.com>
2022-08-08 12:58:13 -0500

    Adding login form

--- client/src/app/modules/navigation/navigation.component.ts
+++ client/src/app/modules/navigation/navigation.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
+import { OverlayPanel } from 'primeng/overlaypanel';
 import { ILoginRequest } from 'src/app/models/requests/login-request';
 import { UserDataService } from 'src/app/services/data/user-data.service';
 
@@ -13,8 +15,21 @@ export class NavigationComponent implements OnInit {
   items: Array<MenuItem>;
   userItems: Array<MenuItem>;
   loggedIn: boolean = false;
+  loginFormGroup: FormGroup;
+  loginNameKey = 'loginName';
+  passwordKey = 'password';
+
+  @ViewChild('op')
+  loginPanel: OverlayPanel | undefined
 
   constructor(private router: Router, private userDataService: UserDataService) {
+    this.loginFormGroup = new FormGroup(
+      {
+        loginName: new FormControl(''),
+        password: new FormControl('')
+      },
+    );
+
     this.items = [
       {
         label: 'Home',
@@ -44,12 +59,14 @@ export class NavigationComponent implements OnInit {
 
   login() {
     const request = {
-      name: "some_one",
-      password: "password1"
+      name: this.loginFormGroup.get(this.loginNameKey)?.value,
+      password: this.loginFormGroup.get(this.passwordKey)?.value,
     } as ILoginRequest;
 
     this.userDataService.login(request).subscribe(() => {
       this.loggedIn = true;
+
+      this.loginPanel?.hide();
     });
   }
 
--- client/src/app/modules/navigation/navigation.module.ts
+++ client/src/app/modules/navigation/navigation.module.ts
@@ -1,15 +1,29 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavigationComponent } from './navigation.component';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 import { MenuModule } from 'primeng/menu';
 import { MenubarModule } from 'primeng/menubar';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 import { SplitButtonModule } from 'primeng/splitbutton';
-import { ButtonModule } from 'primeng/button';
-import { AvatarModule } from 'primeng/avatar';
+import { NavigationComponent } from './navigation.component';
 
 @NgModule({
   declarations: [NavigationComponent],
   exports: [NavigationComponent],
-  imports: [CommonModule, MenubarModule, ButtonModule, SplitButtonModule, AvatarModule, MenuModule],
+  imports: [
+    CommonModule,
+    MenubarModule,
+    ButtonModule,
+    SplitButtonModule,
+    AvatarModule,
+    MenuModule,
+    OverlayPanelModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
 })
 export class NavigationModule {}

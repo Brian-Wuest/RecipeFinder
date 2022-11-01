ad00125053ed6a6bd272993db4e7032fd72afd00
Brian Wuest <brian.wuest@outlook.com>
2022-11-01 06:27:46 -0500

    Making parsing of user id safer

--- server/src/api/middleware/authorization_middlware.rs
+++ server/src/api/middleware/authorization_middlware.rs
@@ -16,24 +16,25 @@ impl AuthorizationMiddlware {
 		let some_identity = req.extract::<Identity>().await;
 
 		if let Ok(found_identity) = some_identity {
-			let user_id = Uuid::parse_str(&found_identity.id().unwrap()).unwrap();
-			let data_result = ApplicationRole::get_highest_role_for_user(&user_id).await;
+			if let Ok(user_id) = Uuid::parse_str(&found_identity.id().unwrap()) {
+				let data_result = ApplicationRole::get_highest_role_for_user(&user_id).await;
 
-			if let Some(found_result) = data_result {
-				if found_result.classification >= 1 {
-					return_value.push(BASIC.to_string());
-				}
+				if let Some(found_result) = data_result {
+					if found_result.classification >= 1 {
+						return_value.push(BASIC.to_string());
+					}
 
-				if found_result.classification >= 2 {
-					return_value.push(POWER_USER.to_string());
-				}
+					if found_result.classification >= 2 {
+						return_value.push(POWER_USER.to_string());
+					}
 
-				if found_result.classification >= 4 {
-					return_value.push(CAT_ADMIN.to_string());
-				}
+					if found_result.classification >= 4 {
+						return_value.push(CAT_ADMIN.to_string());
+					}
 
-				if found_result.classification >= 256 {
-					return_value.push(SYS_ADMIN.to_string());
+					if found_result.classification >= 256 {
+						return_value.push(SYS_ADMIN.to_string());
+					}
 				}
 			}
 		}
